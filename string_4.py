e7f66441dc8a91c1c1996516815a6e89cc850521
CHARISHMAA070217 <charishma7118@gmail.com>
2024-10-22 11:11:20 +0530

    Create string_4.py

---
+++ string_4.py
@@ -0,0 +1,18 @@
+'''
+ Write a  Python Program to calculate the number of words and characters present in a string.
+Input & Output Format:
+Input consists of a string.
+Output consists of two integers.
+First output refers to the count of the words.
+Second output refers to the count of the characters in a given string,
+Sample Input:
+Cyfuno
+Sample Output:
+Words: 1
+Letters: 6
+test_string = "Kcherry"
+ print("The original string is : " + test_string)
+res = len(test_string.split())
+print("The number of words in string are : " + str(res))
+print("The number of words in string are : ", len(test_string))
+'''
