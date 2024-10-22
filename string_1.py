b4bae1952c3c4a66241095c74d2222577e5458a1
CHARISHMAA070217 <charishma7118@gmail.com>
2024-10-22 10:53:59 +0530

    Create string_1.py

---
+++ string_1.py
@@ -0,0 +1,15 @@
+'''
+1) Write a program to get a string value and print the same as output:
+Sample Input:
+Hello World
+Sample Output:
+Hello World
+Note: There is no newline after printing the string values.
+
+n = "HELLO WORLD"
+s = str(n)
+
+print(s)
+output:
+    HELLO WORLD
+'''
